7aa1f7f20c61b3b80f237c20571d44ea25eeb552
vikash906096 <vikashcprinfo@gmail.com>
2026-03-25 20:52:11 +0530

    i havee editet product .html fille

---
+++ test.js
@@ -0,0 +1,45 @@
+console.log("hello");
+
+const products = document.getElementById("produt");
+
+async function prodeuts() {
+  try {
+    const respose = await fetch("test.json");
+    const data = await respose.json();
+    console.log(data);
+
+    data.products.forEach((item) => {
+      data.categories.forEach((item) => console.log(item));
+      const cards = document.createElement("div");
+      cards.classList.add = "card";
+      cards.innerHTML += `
+
+      
+        <div class="card">
+              <img src="${item.imageUrl}" class="card-img-top" alt="imgde">
+              <div class"card-text>
+
+
+              <p class="title"> rating: ${item.rating}</p>
+              <p class="brand"> brand: ${item.brand}</p>
+              <p class="category"> Category: ${item.category}</p>
+              <p class="subcat">${item.subcategory}</p>
+              
+
+              <h4 class="price"> Prie: ${item.price}</h4>
+              <p>${item.description}</p>
+              <div class="btn">
+               <a href="second.html?id=${item.id}" class="btn">view</a>
+               </div>
+               
+       </div>
+        </div>
+    
+    `;
+      products.appendChild(cards);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+prodeuts();
